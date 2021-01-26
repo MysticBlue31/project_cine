adc09f897f562a855e3c76455f2eb395778b4cc6
HyeonYongLee <HyeonYongLee.dev@gmail.com>
2021-01-26 10:03:02 +0900

    1/26 IUserDao, IUservice, LoginDto, User_Mapper, UserController, UserDao, UserService

--- project_cinephile/src/main/java/com/project/cine/service/IUserService.java
+++ project_cinephile/src/main/java/com/project/cine/service/IUserService.java
@@ -1,7 +1,10 @@
 package com.project.cine.service;
+import com.project.cine.dtos.LoginDto;
 import com.project.cine.dtos.UserDto;
 
 public interface IUserService {
 	//회원 가입 메서드
 	void register(UserDto userDto) throws Exception;
+	//로그인 처리
+	UserDto login(LoginDto loginDto) throws Exception;
 }
